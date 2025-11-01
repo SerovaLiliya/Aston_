d34147c8c81240bb84ad2533fc274e3c2bc99365
Liliya Serova <ulitka-63@yandex.ru>
2025-11-01 19:03:29 +0300

    Lesson_3_

---
+++ src/test/java/Lesson_7/FactorialCalculatorTest.java
@@ -0,0 +1,22 @@
+package Lesson_7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialCalculatorTest {
+
+    @Test
+    void testFactorialOf5() {
+        assertEquals(120, FactorialCalculator.factorial(5));
+    }
+
+    @Test
+    void testFactorialOf0() {
+        assertEquals(1, FactorialCalculator.factorial(0));
+    }
+
+    @Test
+    void testNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.factorial(-1));
+    }
+}
